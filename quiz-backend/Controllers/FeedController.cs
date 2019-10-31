242362b60db8e5c2007bd45334b3fccd24cd9734
Marvin Zumbado <marvinzzz@gmail.com>
2019-10-30 21:42:08 -0600

    first load

---
+++ quiz-backend/Controllers/FeedController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using quiz_backend;
+using quiz_backend.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Feed")]
+    public class FeedController : Controller
+    {
+        private readonly QuizContext _context;
+
+        public FeedController(QuizContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("GetAllFeeds")]
+        public IEnumerable<Feed> GetAllFeeds()
+        {
+            return _context.Feed;
+        }
+
+        [HttpPost("GetMyFeeds")]
+        public IEnumerable<Feed> GetMyFeeds([FromBody] SimpleUser simpleUSer)
+        {
+            var myFeeds = _context.UserFeed.Where(e => e.UserId == simpleUSer.UserId).ToList();
+            var allFeeds = _context.Feed.ToList();
+            foreach (var feed in allFeeds)
+            {
+                if (myFeeds.Any(e => e.FeedId == feed.Id))
+                {
+                    feed.isSubscribed = true;
+                }
+            }
+            return allFeeds;
+        }
+
+
+        [HttpPost("SubscribeUnsubscribe")]
+        public bool SubscribeUnsubscribe([FromBody] UserFeed uf)
+        {
+            var relationship = _context.UserFeed.SingleOrDefault(m => m.UserId == uf.UserId && m.FeedId == uf.FeedId);
+            if (relationship == null)
+            {
+                _context.UserFeed.Add(new UserFeed { FeedId = uf.FeedId, UserId = uf.UserId });
+            }
+            else
+            {
+                _context.UserFeed.Remove(relationship);
+
+            }
+            _context.SaveChanges();
+            return true;
+        }
+
+
+    }
+}
---
+++ quiz-backend/Models/SampleData.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quiz_backend.Models
+{
+    public class SampleData
+    {
+        private UserDbContext _context;
+        private QuizContext _context2;
+
+
+        public SampleData(UserDbContext context, QuizContext context2)
+        {
+            _context = context;
+            _context2 = context2;
+        }          
+
+
+        public async void SeedAdminUser()
+        {
+            var user = new IdentityUser
+            {
+                UserName = "Email@email.com",
+                NormalizedUserName = "Email@email.com",
+                Email = "Email@email.com",
+                NormalizedEmail = "email@email.com",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString()
+
+            };
+
+            var roleStore = new RoleStore<IdentityRole>(_context);
+
+            if (!_context.Roles.Any(r => r.Name == "admin"))
+            {
+                await roleStore.CreateAsync(new IdentityRole { Name = "admin", NormalizedName = "admin" });
+            }
+
+            if (!_context.Users.Any(u => u.UserName == user.UserName))
+            {
+                var password = new PasswordHasher<IdentityUser>();
+                var hashed = password.HashPassword(user, "hola");
+                user.PasswordHash = hashed;
+                var userStore2 = new UserStore<IdentityUser>(_context);
+                await userStore2.CreateAsync(user);
+                await userStore2.AddToRoleAsync(user, "admin");
+            }
+
+            await _context.SaveChangesAsync();
+
+
+            var userStore = new UserStore<IdentityUser>(_context);
+            var password2 = new PasswordHasher<IdentityUser>();
+            var hashed2 = password2.HashPassword(user, "hola");
+
+            IdentityUser user0, user1;
+            if (_context.Users.Any(u => u.UserName == "nemo@mail.com"))
+                user0 = _context.Users.First(u => u.UserName == "nemo@mail.com");
+            else
+            {
+                user0 = new IdentityUser { UserName = "nemo@mail.com", Email = "nemo@mail.com", PasswordHash = hashed2 };
+                var result = userStore.CreateAsync(user0).Result;
+            }
+            if (_context.Users.Any(u => u.UserName == "teresa@mail.com"))
+                user1 = _context.Users.First(u => u.UserName == "teresa@mail.com");
+            else
+            {
+                user1 = new IdentityUser { UserName = "teresa@mail.com", Email = "teresa@mail.com", PasswordHash = hashed2 };
+                var result = userStore.CreateAsync(user1).Result;
+            }
+                                 
+
+        }
+
+        public async void SeedFeeds()
+        {
+            var tempList = new List<Feed>();
+
+            var feed1 = new Feed
+            {
+                Title = "Top Headlines",
+                url = "https://www.espn.com/espn/rss/news"
+            };
+            tempList.Add(feed1);
+
+            var feed2 = new Feed
+            {
+                Title = "NFL",
+                url = "https://www.espn.com/espn/rss/nfl/news"
+            };
+            tempList.Add(feed2);
+
+
+            var feed3 = new Feed
+            {
+                Title = "NBA",
+                url = "https://www.espn.com/espn/rss/nba/news"
+            };
+            tempList.Add(feed3);
+
+
+            var feed4 = new Feed
+            {
+                Title = "MLB",
+                url = "https://www.espn.com/espn/rss/mlb/news"
+            };
+            tempList.Add(feed4);
+
+
+            var feed5 = new Feed
+            {
+                Title = "NHL",
+                url = "https://www.espn.com/espn/rss/nhl/news"
+            };
+            tempList.Add(feed5);
+
+
+            var feed6 = new Feed
+            {
+                Title = "Motors",
+                url = "https://www.espn.com/espn/rss/rpm/news"
+            };
+            tempList.Add(feed6);
+
+
+            var feed7 = new Feed
+            {
+                Title = "Soccer",
+                url = "https://www.espn.com/espn/rss/soccer/news"
+            };
+            tempList.Add(feed7);
+
+
+            var feed8 = new Feed
+            {
+                Title = "U",
+                url = "https://www.espn.com/espn/rss/espnu/news"
+            };
+            tempList.Add(feed8);
+
+
+            var feed9 = new Feed
+            {
+                Title = "NCAAB",
+                url = "https://www.espn.com/espn/rss/ncb/news"
+            };
+            tempList.Add(feed9);
+
+
+            var feed10 = new Feed
+            {
+                Title = "NCAAF",
+                url = "https://www.espn.com/espn/rss/ncf/news"
+            };
+            tempList.Add(feed10);
+
+            var feed11 = new Feed
+            {
+                Title = "Action",
+                url = "https://www.espn.com/espn/rss/action/news"
+            };
+            tempList.Add(feed10);
+
+            var feed12 = new Feed
+            {
+                Title = "Poker",
+                url = "https://www.espn.com/espn/rss/poker/master"
+            };
+            tempList.Add(feed10);
+
+
+
+            foreach (var fd in tempList)
+            {
+                if (!_context2.Feed.Any(u => u.Title == fd.Title))
+                {
+                    _context2.Feed.Add(fd);
+                }
+
+            }
+            await _context2.SaveChangesAsync();
+
+        }
+  
+    }
+}
